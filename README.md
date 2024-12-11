d3127e734689f691bf0e5aa21b6a74f55256fb25
quangster <buiduyquang2k4@gmail.com>
2024-12-11 16:03:19 +0700

    update pickle dataset

--- README.md
+++ README.md
@@ -9,8 +9,33 @@ Deep Learning Project
 
 ## Installation
 
-To install the required libraries and download the dataset, run the following command:
 
 ```bash
-python setup.py
+# clone project
+git clone https://github.com/quangster/machine-translation
+cd machine-translation
+
+# [OPTIONAL] create conda environment
+conda create -n myenv python=3.10
+conda activate myenv
+
+# install pytorch according to instructions
+# https://pytorch.org/get-started/locally/
+
+pip install -r requirements.txt
+```
+
+## Download dataset
+```bash
+gdown --folder 1cPdLNnTlsj3N1FE9x6_K608bCAaYaVGM -O data
+```
+
+## Build vocabulary and dataset
+```bash
+"""
+This function processes the given dataset and vocabulary files. 
+Note that the vocab and dataset files have already been downloaded from Google Drive, so there is no need to re-run the command to download them. 
+Please be aware that running this command might take a significant amount of time.
+"""
+python src/vocab.py
 ```
--- src/vocab.py
+++ src/vocab.py
@@ -1,11 +1,16 @@
 import rootutils
 from tqdm import tqdm
+import pickle
+
 rootutils.setup_root(__file__, indicator=".project-root", pythonpath=True)
 
 from src.utils.data import read_corpus  # noqa: E402
 from src.utils.other import print_message  # noqa: E402
 from src.data import Vocabulary, ViTokenizer, EnTokenizer  # noqa: E402
 
+def save_dataset(file_path, en_ids, vi_ids):
+    with open(file_path, "wb") as f:
+        pickle.dump((en_ids, vi_ids), f)
 
 if __name__ == "__main__":
     DATA_DIR = "data"
@@ -41,7 +46,25 @@ if __name__ == "__main__":
     vi_vocab = Vocabulary.from_corpus(all_vi_sents, size=24270, min_freq=20, language="vietnamese")
 
     print_message("Saving vocabularies ...")
-    en_vocab.save("ckpts/en_vocab.json")
-    vi_vocab.save("ckps/vi_vocab.json")
+
+    en_vocab.save("./ckpts/en_vocab.json")
+    vi_vocab.save("./ckpts/vi_vocab.json")
+
+    print_message("Build dataset ...")
+
+    train_en_ids = [en_vocab.words2indexes(sent, add_sos_eos=True) for sent in tqdm(train_en_sents)]
+    train_vi_ids = [vi_vocab.words2indexes(sent, add_sos_eos=True) for sent in tqdm(train_vi_sents)]
+
+    dev_en_ids = [en_vocab.words2indexes(sent, add_sos_eos=True) for sent in tqdm(dev_en_sents)]
+    dev_vi_ids = [vi_vocab.words2indexes(sent, add_sos_eos=True) for sent in tqdm(dev_vi_sents)]
+
+    test_en_ids = [en_vocab.words2indexes(sent, add_sos_eos=True) for sent in tqdm(test_en_sents)]
+    test_vi_ids = [vi_vocab.words2indexes(sent, add_sos_eos=True) for sent in tqdm(test_vi_sents)]
+
+    print_message("Saving dataset ...")
+
+    save_dataset("./data/train.pkl", train_en_ids, train_vi_ids)
+    save_dataset("./data/dev.pkl", dev_en_ids, dev_vi_ids)
+    save_dataset("./data/test.pkl", test_en_ids, test_vi_ids)
 
     print_message("Done!")
